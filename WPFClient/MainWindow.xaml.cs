1fb3002acd13bb45e146f4660e5e11eecfdfa0fa
hjortron <percunium@gmail.com>
2015-04-02 10:49:02 +0300

    Change all

---
+++ WPFClient/MainWindow.xaml.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Windows;
+using Microsoft.AspNet.SignalR.Client;
+
+namespace WPFClient
+{   
+    /// <summary>
+    /// SignalR client hosted in a WPF application. The client
+    /// lets the user pick a user name, connect to the server asynchronously
+    /// to not block the UI thread, and send chat messages to all connected 
+    /// clients whether they are hosted in WinForms, WPF, or a web application.
+    /// For simplicity, MVVM will not be used for this sample.
+    /// </summary>
+    public partial class MainWindow
+    {
+        /// <summary>
+        /// This name is simply added to sent messages to identify the user; this 
+        /// sample does not include authentication.
+        /// </summary>
+
+        private const byte VkVolumeMute = 0xAD; //Volume Mute key
+        private const byte VkVolumeUp = 0xAF; //Volume Up key
+        private const byte VkVolumeDown = 0xAE; //Volume Down key
+        private const byte VkMediaNextTrack = 0xB0; //Next Track key
+        private const byte VkMediaPrevTrack = 0xB1; //Previous Track key
+        private const byte VkMediaStop = 0xB2; //Stop Media key
+        private const byte VkMediaPlayPause = 0xB3; //Play/Pause Media key
+
+        public string UserName { get; set; }
+        public IHubProxy HubProxy { get; set; }
+     
+        public HubConnection Connection { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonSend_Click(object sender, RoutedEventArgs e)
+        {
+            HubProxy.Invoke("Send", UserName, TextBoxMessage.Text);
+            TextBoxMessage.Text = string.Empty;
+            TextBoxMessage.Focus();
+        }
+
+        /// <summary>
+        /// Creates and connects the hub connection and hub proxy. This method
+        /// is called asynchronously from SignInButton_Click.
+        /// </summary>
+        private async void ConnectAsync(string serverIp)
+        {          
+            Connection = new HubConnection($"http://{serverIp}:8080");
+            Connection.Closed += Connection_Closed;
+            HubProxy = Connection.CreateHubProxy("MyHub");
+            //Handle incoming event from server: use Invoke to write to console from SignalR's thread
+            HubProxy.On<string, string>("AddMessage", (name, message) =>
+                Dispatcher.Invoke(() =>
+                    RichTextBoxConsole.AppendText($"{name}: {message}\r")
+                )
+            );
+            try
+            {
+                await Connection.Start();
+            }
+            catch (HttpRequestException)
+            {
+                StatusText.Content = "Unable to connect to server: Start server before connecting clients.";
+                //No connection: Don't enable Send button or show chat UI
+                return;
+            }
+
+            //Show chat UI; hide login UI
+            SignInPanel.Visibility = Visibility.Collapsed;
+            ChatPanel.Visibility = Visibility.Visible;
+            ButtonSend.IsEnabled = true;
+            TextBoxMessage.Focus();
+            RichTextBoxConsole.AppendText("Connected to server at " + serverIp + "\r");
+        }
+
+        /// <summary>
+        /// If the server is stopped, the connection will time out after 30 seconds (default), and the 
+        /// Closed event will fire.
+        /// </summary>
+        void Connection_Closed()
+        {
+            //Hide chat UI; show login UI
+            var dispatcher = Application.Current.Dispatcher;
+            dispatcher.Invoke(() => ChatPanel.Visibility = Visibility.Collapsed);
+            dispatcher.Invoke(() => ButtonSend.IsEnabled = false);
+            dispatcher.Invoke(() => StatusText.Content = "You have been disconnected.");
+            dispatcher.Invoke(() => SignInPanel.Visibility = Visibility.Visible);
+        }
+
+        private void SignInButton_Click(object sender, RoutedEventArgs e)
+        {
+            var hostEntry = Dns.GetHostEntry(UserNameTextBox.Text);
+            if (hostEntry.AddressList.Length == 0) return;
+            var ip = hostEntry.AddressList[2].ToString();        
+            //Connect to server (use async method to avoid blocking UI thread)
+            if (string.IsNullOrEmpty(ip)) return;
+            StatusText.Visibility = Visibility.Visible;
+            StatusText.Content = "Connecting to server...";
+            ConnectAsync(ip);
+        }
+
+        private void WPFClient_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (Connection == null) return;
+            Connection.Stop();
+            Connection.Dispose();
+        }
+
+        private void PreviousButton_Click(object sender, RoutedEventArgs e)
+        {
+            HubProxy.Invoke("Send", VkMediaPrevTrack);
+        }
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            HubProxy.Invoke("Send", VkMediaStop);
+        }
+
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            HubProxy.Invoke("Send", VkMediaPlayPause);
+        }
+
+        private void NextButton_Click(object sender, RoutedEventArgs e)
+        {
+            HubProxy.Invoke("Send", VkMediaNextTrack);
+        }
+    }
+}
---
+++ WPFServer/MainWindow.xaml.cs
@@ -0,0 +1,160 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.Hosting;
+using Owin;
+using System;
+using System.Net;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WPFServer
+{
+    /// <summary>
+    /// WPF host for a SignalR server. The host can stop and start the SignalR
+    /// server, report errors when trying to start the server on a URI where a
+    /// server is already being hosted, and monitor when clients connect and disconnect. 
+    /// The hub used in this server is a simple echo service, and has the same 
+    /// functionality as the other hubs in the SignalR Getting Started tutorials.
+    /// For simplicity, MVVM will not be used for this sample.
+    /// </summary>
+    public partial class MainWindow
+    {        
+        const int KEYEVENTF_KEYDOWN = 0x0;
+        const uint KEYEVENTF_EXTENDEDKEY = 0x0001;
+        const uint KEYEVENTF_KEYUP = 0x0002;
+        public IDisposable SignalR { get; set; }       
+
+        public MainWindow()
+        {
+            InitializeComponent();
+          
+        }
+        [DllImport("user32.dll")]
+        static extern UInt32 SendInput(UInt32 nInputs, [MarshalAs(UnmanagedType.LPArray, SizeConst = 1)] INPUT[] pInputs, Int32 cbSize);
+        [DllImport("user32.dll", EntryPoint = "keybd_event", CharSet = CharSet.Auto, ExactSpelling = true)]
+        private static extern void keybd_event(byte vk, byte bscan, uint flags, int extrainfo);     
+        
+        public void Press(byte key)
+         {
+            INPUT[] InputData = new INPUT[1];
+
+            InputData[0].Type = (UInt32)InputType.KEYBOARD;
+            //InputData[0].Vk = (ushort)DirectInputKeyScanCode;  //Virtual key is ignored when sending scan code
+            InputData[0].Scan = (ushort)DirectInputKeyScanCode;
+            InputData[0].Flags = (uint)KeyboardFlag.KEYUP | (uint)KeyboardFlag.SCANCODE;
+            InputData[0].Time = 0;
+            InputData[0].ExtraInfo = IntPtr.Zero;
+
+            // Send Keyup flag "OR"ed with Scancode flag for keyup to work properly
+            SendInput(1, InputData, Marshal.SizeOf(typeof (INPUT)));
+         }
+
+    /// <summary>
+    /// Calls the StartServer method with Task.Run to not
+    /// block the UI thread. 
+    /// </summary>
+    private void ButtonStart_Click(object sender, RoutedEventArgs e)
+        {
+            var hostEntry = Dns.GetHostEntry(Dns.GetHostName());
+
+            if (hostEntry.AddressList.Length == 0) return;
+            var ip = hostEntry.AddressList[2].ToString();
+            WriteToConsole("Starting server...");
+            ButtonStart.IsEnabled = false;            
+            Task.Run(() => StartServer(ip));
+        }
+
+        /// <summary>
+        /// Stops the server and closes the form. Restart functionality omitted
+        /// for clarity.
+        /// </summary>
+        private void ButtonStop_Click(object sender, RoutedEventArgs e)
+        {
+            SignalR.Dispose();
+            Close();
+        }
+
+        /// <summary>
+        /// Starts the server and checks for error thrown when another server is already 
+        /// running. This method is called asynchronously from Button_Start.
+        /// </summary>
+        private void StartServer(string serverIp)
+        {
+            try
+            {
+                SignalR = WebApp.Start($"http://{serverIp}:8080");
+            }
+            catch (TargetInvocationException)
+            {
+                WriteToConsole("A server is already running at " + serverIp);
+                Dispatcher.Invoke(() => ButtonStart.IsEnabled = true);
+                return;
+            }
+            Dispatcher.Invoke(() => ButtonStop.IsEnabled = true);
+            WriteToConsole("Server started at " + serverIp);
+        }
+        ///This method adds a line to the RichTextBoxConsole control, using Dispatcher.Invoke if used
+        /// from a SignalR hub thread rather than the UI thread.
+        public void WriteToConsole(string message)
+        {
+            if (!(RichTextBoxConsole.CheckAccess()))
+            {
+                Dispatcher.Invoke(() =>
+                    WriteToConsole(message)
+                );
+                return;
+            }
+            RichTextBoxConsole.AppendText(message + "\r");
+        }
+
+        private void ButtonNext_Click(object sender, RoutedEventArgs e)
+        {
+            Press(0xB0);
+        }
+    }
+    /// <summary>
+    /// Used by OWIN's startup process. 
+    /// </summary>
+    class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCors(CorsOptions.AllowAll);
+            app.MapSignalR();
+        }
+    }
+    /// <summary>
+    /// Echoes messages sent using the Send message by calling the
+    /// addMessage method on the client. Also reports to the console
+    /// when clients connect and disconnect.
+    /// </summary>
+    public class MyHub : Hub
+    {
+        public void Send(byte keyCode)
+        {
+            Application.Current.Dispatcher.Invoke(() =>
+                ((MainWindow)Application.Current.MainWindow).Press(keyCode));
+        }
+        public override Task OnConnected()
+        {
+            //Use Application.Current.Dispatcher to access UI thread from outside the MainWindow class
+            Application.Current.Dispatcher.Invoke(() => 
+                ((MainWindow)Application.Current.MainWindow).WriteToConsole("Client connected: " + Context.ConnectionId));
+
+            return base.OnConnected();
+        }
+        public override Task OnDisconnected()
+        {
+            //Use Application.Current.Dispatcher to access UI thread from outside the MainWindow class
+            Application.Current.Dispatcher.Invoke(() => 
+                ((MainWindow)Application.Current.MainWindow).WriteToConsole("Client disconnected: " + Context.ConnectionId));
+
+            return base.OnDisconnected();
+        }
+
+    }
+}
